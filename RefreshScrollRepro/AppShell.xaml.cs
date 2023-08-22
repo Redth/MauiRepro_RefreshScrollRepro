b37d3830366a76292c44868a6d7d4e2076f4aa3f
redth <jondick@gmail.com>
2023-08-22 10:25:10 -0400

    Added repro

---
+++ RefreshScrollRepro/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RefreshScrollRepro;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ RefreshScrollRepro/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace RefreshScrollRepro;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    async void RefreshView_Refreshing(System.Object sender, System.EventArgs e)
+    {
+		await Task.Delay(2000);
+
+		this.refreshView.IsRefreshing = false;
+    }
+}
+
+
